e960747acfcd0089664b4030a844e87413b6ec45
Ankit <ankitdj13112002@gmail.com>
2024-07-04 03:20:28 +0530

    954B

---
+++ 954B.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <bitset>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+#define ll long long
+#define mp make_pair
+#define mod 1000000007
+
+void ankit7890()
+{
+    int n, m;
+    cin >> n >> m;
+    ll a[105][105] = {0};
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            int mx = max(a[i + 1][j], max(a[i - 1][j], max(a[i][j + 1], a[i][j - 1])));
+            cout << (a[i][j] > mx ? mx : a[i][j]) << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test_7890;
+    cin >> test_7890;
+
+    while (test_7890--)
+    {
+        ankit7890();
+    }
+
+    return 0;
+}
