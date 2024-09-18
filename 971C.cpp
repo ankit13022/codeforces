495be714c32fe5b5fad7ce7e26f4cfa4987490a6
Ankit <ankitdj13112002@gmail.com>
2024-09-18 10:56:00 +0530

    971C

---
+++ 971C.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <bitset>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+#define ll long long
+#define mp make_pair
+#define mod 1000000007
+
+void ankit7890()
+{
+    int x, y, k;
+    cin >> x >> y >> k;
+    int ans = max(2 * ((x + k - 1) / k) - 1, 2 * ((y + k - 1) / k));
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test_7890;
+    cin >> test_7890;
+
+    while (test_7890--)
+    {
+        ankit7890();
+    }
+
+    return 0;
+}
