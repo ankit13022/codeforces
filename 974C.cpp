37b53633e33dbf0672d2e496a12527a67a2f1325
Ankit <ankitdj13112002@gmail.com>
2024-11-01 19:15:33 +0530

    974c

---
+++ 974C.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <bitset>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+#define ll long long
+#define mp make_pair
+#define um unordered_map
+#define mod 1000000007
+
+void ankit7890()
+{
+    ll n;
+    cin >> n;
+    vector<ll> arr(n);
+    ll sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    sort(arr.begin(), arr.end());
+    if (n == 1 || n == 2)
+        cout << -1 << endl;
+    else
+    {
+        ll res = 2 * n * arr[n / 2] - sum + 1;
+        cout << max(0LL, res) << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test_7890;
+    cin >> test_7890;
+
+    while (test_7890--)
+    {
+        ankit7890();
+    }
+
+    return 0;
+}
