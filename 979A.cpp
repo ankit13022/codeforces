2d89c1d6b639bad39cd242b983a63527978df768
Ankit <ankitdj13112002@gmail.com>
2024-12-30 19:10:05 +0530

    979A

---
+++ 979A.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <bitset>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+#define ll long long
+#define mp make_pair
+#define um unordered_map
+#define mod 1000000007
+
+void ankit7890()
+{
+    cin >> n;
+    int minx = 10000, maxx = -10000;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        minx = min(minx, x);
+        maxx = max(maxx, x);
+    }
+    cout << (maxx - minx) * (n - 1) << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test_7890;
+    cin >> test_7890;
+
+    while (test_7890--)
+    {
+        ankit7890();
+    }
+
+    return 0;
+}
