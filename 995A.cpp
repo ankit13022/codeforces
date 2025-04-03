4c8dc2c3a5bae95a0822e3d26c1ec93506e5bbe2
Ankit <ankitdj13112002@gmail.com>
2025-04-03 13:48:58 +0530

    995A

---
+++ 995A.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <bitset>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+#define ll long long
+#define mp make_pair
+#define um unordered_map
+#define mod 1000000007
+
+void ankit7890()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    int sum1 = 0;
+    int sum2 = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i] > b[i + 1])
+        {
+            sum1 += a[i];
+            sum2 += b[i + 1];
+        }
+    }
+    sum1 += a[n - 1];
+    cout << sum1 - sum2 << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test_7890;
+    cin >> test_7890;
+
+    while (test_7890--)
+    {
+        ankit7890();
+    }
+
+    return 0;
+}
