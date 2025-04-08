49ad8d856639915d45aff6b534b029eadd9d9699
Ankit <ankitdj13112002@gmail.com>
2025-04-08 19:24:15 +0530

    995A

---
+++ 996A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <bitset>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+#define ll long long
+#define mp make_pair
+#define um unordered_map
+#define mod 1000000007
+
+void ankit7890()
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    int dst;
+    if (a > b)
+        dst = a - b + 1;
+    else
+        dst = b - a + 1;
+    if (dst % 2 == 1)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test_7890;
+    cin >> test_7890;
+
+    while (test_7890--)
+    {
+        ankit7890();
+    }
+
+    return 0;
+}
