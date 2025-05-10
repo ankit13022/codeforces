256e824fb0d75b936ad1272ac63af66febde2b5d
Ankit <ankitdj13112002@gmail.com>
2025-05-10 17:00:15 +0530

    1006B

---
+++ 1006B.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <bitset>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+#define ll long long
+#define mp make_pair
+#define um unordered_map
+#define mod 1000000007
+
+void ankit7890()
+{
+    int n;
+    string s;
+    cin >> n >> s;
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cnt += (s[i] == '-');
+    }
+    cout << (cnt / 2) * (cnt - cnt / 2) * (n - cnt) << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test_7890;
+    cin >> test_7890;
+
+    while (test_7890--)
+    {
+        ankit7890();
+    }
+
+    return 0;
+}
