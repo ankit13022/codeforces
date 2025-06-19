ec8448a866a5a96e23e6fb24a1e4e835959be577
Ankit <ankitdj13112002@gmail.com>
2025-06-19 11:01:55 +0530

    1013B

---
+++ 1013B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <bitset>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+#define ll long long
+#define mp make_pair
+#define um unordered_map
+#define mod 1000000007
+
+void ankit7890()
+{
+    int n;
+    long long x;
+    cin >> n >> x;
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    int teams = 0;
+    int cnt = 0;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        cnt++;
+        if (static_cast<long long>(cnt) * a[i] >= x)
+        {
+            teams++;
+            cnt = 0;
+        }
+    }
+    cout << teams << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test_7890;
+    cin >> test_7890;
+
+    while (test_7890--)
+    {
+        ankit7890();
+    }
+
+    return 0;
+}
